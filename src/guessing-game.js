8538e0d72026b8fd994dcd1b5455f67b89efd891
Alexei <avenge.now.r@mail.ru>
2022-08-19 12:56:01 +0300

    Task Complete!

--- src/guessing-game.js
+++ src/guessing-game.js
@@ -1,20 +1,30 @@
 class GuessingGame {
-    constructor() {}
+    constructor() 
+    {
+        this.root = 0;
+        this.max = null;
+        this.min = null;
+    }
 
     setRange(min, max) {
-
+        this.max = max;
+        this.min = min;
+        this.root = 0;
     }
 
     guess() {
+        let rNum = Math.round((this.max + this.min) / 2);
+        this.root = rNum;
 
+        return rNum;
     }
 
     lower() {
-
+        this.max = this.root;
     }
 
     greater() {
-
+        this.min = this.root;
     }
 }
 
